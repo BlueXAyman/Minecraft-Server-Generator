8d1365a0f75725186e7f0d2262a506c0f2ee1419
xAyman <73948427+BlueXAyman@users.noreply.github.com>
2022-11-03 13:13:05 +0100

    Added more features
    + You can now select what you want to name the folder
    + If the folder with the name you entered already exists, you may choose if you want to delete and regenerate new one, start the server or pick a new name
    + The eula and start files are now generated by the script it self to avoid pulling files from non known websites
    = Fixed a missnamed label

--- generator.bat
+++ generator.bat
@@ -18,6 +18,10 @@ ECHO.
 color b
 ECHO NOTE: This script will generate a folder in the same directory as this script file.
 ECHO.
+
+:: version picker section
+
+
 ECHO Type 1 to Generate 1.8.9 Paper Server
 ECHO Type 2 to Generate 1.19.2 Paper Server
 
@@ -25,36 +29,92 @@ set choice=
 set /p choice=Enter: 
 if not '%choice%'=='' set choice=%choice:~0,1%
 if '%choice%'=='1' goto oneeight
-if '%choice%'=='2' goto onetwelve
+if '%choice%'=='2' goto onenineteen
 ECHO "%choice%" is not valid, try again
 ECHO.
 goto start
 
+:: 1.8.9 section
+
+
 :oneeight
+ECHO.
+ECHO What would you like to name the 1.8.9 server folder? (Leave empty for Server-1.8.9)
+set name=
+set /p name=Enter Folder Name: 
+if '%name%'=='' set name=Server-1.8.9
 ECHO Generating 1.8.9 Paper Folder
-if not exist "Server-1.8.9" mkdir Server-1.8.9
-cd Server-1.8.9
+if exist "%name%" ECHO The Folder "%name%" already exists.. (D=Delete and regenerate a new folder) (S=Start the existing server) (R=Make a new folder with new name)
+if exist "%name%" goto one-eight-exists
+if not exist "%name%" mkdir %name%
+cd %name%
+ECHO %name% has been created...
 ECHO Downloading paper-1.8.8-445.jar from https://api.papermc.io
 if not exist "server.jar" curl "https://api.papermc.io/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar" --output server.jar
 ECHO Preparing Start.bat file
-if not exist "start.bat" curl "https://cdn.xayman.net/admin/start.bat" --output start.bat
+if not exist "start.bat" echo java -jar server.jar>> start.bat
 if not exist "\Plugins" mkdir Plugins
 goto last
 
-:onetwelve
+:one-eight-exists
+set choice=
+set /p choice=(D/S/R)?:  
+if not '%choice%'=='' set choice=%choice:~0,1%
+if /I '%choice%'=='D' goto delete-one-eight
+if /I '%choice%'=='S' goto last
+if /I '%choice%'=='R' goto oneeight
+ECHO "%choice%" is not valid, try again
+ECHO.
+goto one-eight-exists
+
+:delete-one-eight
+@RD /S /Q "%name%"
+goto oneeight
+
+:: 1.19.2 section
+
+:onenineteen
+ECHO.
+ECHO What would you like to name the 1.19.2 server folder? (Leave empty for Server-1.19.2)
+set name=
+set /p name=Enter Folder Name: 
+if '%name%'=='' set name=Server-1.19.2
 ECHO Generating 1.19.2 Paper Folder
-if not exist "Server-1.19.2" mkdir Server-1.19.2
-cd Server-1.19.2
+if exist "%name%" ECHO The Folder "%name%" already exists.. (D=Delete and regenerate a new folder) (S=Start the existing server) (R=Make a new folder with new name)
+if exist "%name%" goto one-nineteen-exists
+if not exist "%name%" mkdir %name%
+cd %name%
+ECHO %name% has been created...
 ECHO Downloading paper-1.19.2-256.jar from https://api.papermc.io
 if not exist "server.jar" curl "https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds/256/downloads/paper-1.19.2-256.jar" --output server.jar
 ECHO Preparing Start.bat file
-if not exist "start.bat" curl "https://cdn.xayman.net/admin/1.19.2/start.bat" --output start.bat
+if not exist "start.bat" echo "%ProgramFiles%\Java\jdk-17.0.5\bin\java" -jar server.jar --nogui>> start.bat
 if not exist "\Plugins" mkdir Plugins
 goto last
+
+:one-nineteen-exists
+set choice=
+set /p choice=(D/S/R)?:  
+if not '%choice%'=='' set choice=%choice:~0,1%
+if /I '%choice%'=='D' goto delete-one-nineteen
+if /I '%choice%'=='S' goto last
+if /I '%choice%'=='R' goto onenineteen
+ECHO "%choice%" is not valid, try again
+ECHO.
+goto one-nineteen-exists
+
+:delete-one-nineteen
+@RD /S /Q "%name%"
+goto onenineteen
+
+
+:: start server & eula promt
+
 
 :last
 ECHO.
 color a
+if not exist "start.bat" cd %name%
 ECHO Do you want to start the server in this window? (Y/N) 
 
 set choice=
@@ -81,7 +141,7 @@ ECHO.
 
 :startserver
 ECHO Auto Accepting Eula
-if not exist "eula.txt" curl "https://cdn.xayman.net/admin/eula.txt" --output eula.txt
+if not exist "eula.txt" echo eula=true>> eula.txt
 start.bat
 :end
 pause
